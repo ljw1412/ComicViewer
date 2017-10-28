1cdd8b5a152c4f5b64cb9332d2c24a6d8b6d711d
ljw <506644791@qq.com>
2017-10-28 22:15:48 +0800

    fix "UpdateFragment.java" ,The first letter of an identifier is lowercase  in git.

--- app/src/main/java/ljw/comicviewer/ui/fragment/CategoryFragment.java
+++ app/src/main/java/ljw/comicviewer/ui/fragment/CategoryFragment.java
@@ -14,17 +14,13 @@ import ljw.comicviewer.R;
  */
 public class CategoryFragment extends Fragment {
 
-
-    public CategoryFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+
+
+        return view;
     }
 
 }
--- app/src/main/java/ljw/comicviewer/ui/fragment/UpdateFragment.java
+++ app/src/main/java/ljw/comicviewer/ui/fragment/UpdateFragment.java
@@ -14,17 +14,12 @@ import ljw.comicviewer.R;
  */
 public class UpdateFragment extends Fragment {
 
-
-    public UpdateFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false);
+        View view = inflater.inflate(R.layout.fragment_update, container, false);
+
+        return view;
     }
 
 }
